2e891f4ba8611005c9d440c2c23c502cf58682ea
Zune Zune <zunezune@Zunes-MacBook-Pro.local>
2021-08-24 14:31:53 +0630

    Angular Built-In Pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Course } from 'src/app/model/course';
-import {COURSES} from '../db-data';
+import { COURSES } from '../db-data';
 
 @Component({
   selector: 'app-root',
@@ -8,11 +8,20 @@ import {COURSES} from '../db-data';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  courses=COURSES;
+  courses = COURSES;
 
+  startDate = new Date(2000, 0, 1);
 
-  onCourseSelected(course:Course){
-    console.log("App component - button click.....",course);
+  title=COURSES[0].description;
+  
+  price=9.99232424234234234;
+
+  rate=0.67;
+
+  course=COURSES[0];
+
+  onCourseSelected(course: Course) {
+    console.log("App component - button click.....", course);
   }
 
 }
