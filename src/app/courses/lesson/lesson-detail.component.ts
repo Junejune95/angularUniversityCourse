867b4af4612b7b55b5ddd79517045773261fb559
Zune Zune <zunezune@Zunes-MacBook-Pro.local>
2021-08-28 16:38:43 +0630

    Programmatic Router Navigation with the Router Service

--- src/app/courses/lesson/lesson-detail.component.ts
+++ src/app/courses/lesson/lesson-detail.component.ts
@@ -1,8 +1,8 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {ActivatedRoute, Router} from "@angular/router";
-import {LessonDetail} from "../model/lesson-detail";
-import {Observable} from "rxjs";
-import {map} from "rxjs/operators";
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from "@angular/router";
+import { LessonDetail } from "../model/lesson-detail";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
 
 @Component({
   selector: 'lesson',
@@ -12,18 +12,25 @@ import {map} from "rxjs/operators";
 export class LessonDetailComponent implements OnInit {
 
   lesson$: Observable<LessonDetail>;
-  lesson:LessonDetail;
+  lesson: LessonDetail;
 
-  constructor(private route:ActivatedRoute) {
+  constructor(private route: ActivatedRoute,
+    private router: Router) {
 
     console.log("Created LessonDetailComponent...");
 
   }
 
   ngOnInit() {
-   this.lesson= this.route.snapshot.data["lesson"];
+    this.lesson = this.route.snapshot.data["lesson"];
   }
 
+  previous(lesson: LessonDetail) {
+    this.router.navigate(['lessons', lesson.seqNo - 1], { relativeTo: this.route.parent },);
+  }
 
+  next(lesson: LessonDetail) {
+    this.router.navigate(['lessons', lesson.seqNo + 1], { relativeTo: this.route.parent },);
+  }
 
 }
