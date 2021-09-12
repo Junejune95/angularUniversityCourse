416d608ee47c4ec782c317ab29e0e1367631e61b
Zune Zune <zunezune@Zunes-MacBook-Pro.local>
2021-09-12 16:30:40 +0630

    reactive form and template driven form

--- src/app/login-reactive/login-reactive.component.ts
+++ src/app/login-reactive/login-reactive.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { createPasswordStrengthValidator } from '../validators/password-stretnght.validator';
 
 
 @Component({
@@ -7,15 +9,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-reactive.component.css']
 })
 export class LoginReactiveComponent implements OnInit {
+  // email = new FormControl('',
+  //   {
+  //     validators: [Validators.required, Validators.email],
+  //     updateOn: 'blur'
+  //   });
+  // password = new FormControl('',
+  //   {
+  //     validators: [
+  //       Validators.required,
+  //       Validators.minLength(8),
+  //       createPasswordStrengthValidator(),
+  //     ]
+  //   });
 
+  // form = new FormGroup({
+  //   email: this.email,
+  //   password: this.password
+  // });
 
-  constructor() {
+  form = this.fb.group({
+    email: ['', {
+      validators:[Validators.required, Validators.email],
+      updateOn:'blur'
+    }],
+    password: ['', [Validators.required, Validators.minLength(8), createPasswordStrengthValidator(),]]
+  });
+  constructor(private fb: FormBuilder) {
 
 
   }
 
   ngOnInit() {
 
+  }
+
+  get email(){
+    return this.form.controls['email'];
+  }
+
+  get password(){
+    return this.form.controls['password'];
   }
 
 }
